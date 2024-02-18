ef3c4f0332c74828105ebcf14eaa54cde3887a6e
Isaque <isaque.bm9@gmail.com>
2024-02-18 16:58:21 -0300

    feat: functions to create new structs

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,4 @@
+use super::utils;
 pub const DIR_MODE: u8 = 0;
 pub const FILE_MODE: u8 = 1;
 
@@ -18,14 +19,58 @@ pub struct Inode {
     data: InodeData,
 }
 
+impl Inode {
+    pub fn new(mode: u8, name: String) -> Inode {
+        if (mode == DIR_MODE) {
+            Inode {
+                mode,
+                size: 0,
+                permissions: (true, true),
+                created_at: Some(utils::now_date()),
+                updated_at: Some(utils::now_date()),
+                accessed_at: Some(utils::now_date()),
+                data: InodeData::File(File::new(name)),
+            }
+        } else {
+            Inode {
+                mode,
+                size: 0,
+                permissions: (true, true),
+                created_at: Some(utils::now_date()),
+                updated_at: Some(utils::now_date()),
+                accessed_at: Some(utils::now_date()),
+                data: InodeData::Directory(Directory::new(name)),
+            }
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct File {
     name: String,
     data: String,
 }
 
+impl File {
+    pub fn new(name: String) -> File {
+        File {
+            name,
+            data: String::new(),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Directory {
     name: String,
     files: Vec<Inode>,
+}
+
+impl Directory {
+    pub fn new(name: String) -> Directory {
+        Directory {
+            name,
+            files: Vec::new(),
+        }
+    }
 }
